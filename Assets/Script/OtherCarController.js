73d00f4d88035c2cb920f82bcf12e4600825e015
Norman Chen <s965202030@gmail.com>
2016-08-15 01:48:04 +0800

    Init commit.

---
+++ Assets/Script/OtherCarController.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+private var rb : Rigidbody;
+public var speed : int;
+
+function Start () {
+
+	rb = gameObject.GetComponent(Rigidbody) as Rigidbody;
+	var movement: Vector3 = new Vector3 (0, 0, speed * -1);
+	rb.AddForce(movement);
+
+}
+
+function Update () {
+	
+}
